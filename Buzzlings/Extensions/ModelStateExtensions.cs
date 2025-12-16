5b2717362ea612465f6c2250785be7f1aa6d97b9
Caique Assis <caiquesassis@gmail.com>
2025-12-16 18:03:03 -0300

    Added extension methods to ModelState

---
+++ Buzzlings/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Buzzlings.Web.Extensions
+{
+    public static class ModelStateExtensions
+    {
+        public static void AddIdentityErrors(this ModelStateDictionary modelState, IdentityResult result, string keyName)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                modelState.AddModelError(keyName, error.Description);
+            }
+        }
+
+        public static void AddIdentityErrors(this ModelStateDictionary modelState, IEnumerable<IdentityError> errors, string keyName)
+        {
+            foreach (IdentityError error in errors)
+            {
+                modelState.AddModelError(keyName, error.Description);
+            }
+        }
+    }
+}
