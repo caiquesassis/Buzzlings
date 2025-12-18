39aa31112bd3034cc6a80630bb8bcc3f0ec75910
Caique Assis <caiquesassis@gmail.com>
2025-12-18 11:43:27 -0300

    Added extension method to get User id

---
+++ Buzzlings/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace Buzzlings.Web.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string? GetUserId(this ClaimsPrincipal principal)
+        {
+            if (principal.Identity?.IsAuthenticated is false)
+            {
+                return null;
+            }
+
+            return principal.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
